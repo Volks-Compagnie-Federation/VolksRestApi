24f596c17a4892c50ca49d815fbf60f8b73f2ee7
Ren Aldridge <daviddarren220@gmail.com>
2024-06-11 16:00:15 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Ren'// yourname
-global.MONGO_DB_URI = "mongodb+srv://Botwa:daviddarren99@volkscompagnie.nvhbxl7.mongodb.net/?retryWrites=true&w=majority&appName=VolksCompagnie" //database mongodb 
+global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "daviddarren220@gmail.com" //email
-global.email_password = "pfro dqmo ctrh lnre" //application password email
+global.your_email = "" //email
+global.email_password = "" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
